ffd5c9fe6e167171dc15b8025c4d5e88d11ce50b
Flashky <mevengar@gmail.com>
2020-04-30 20:39:54 +0200

    Cambiado scope de test a public

--- src/test/java/brv/telegram/bots/core/restclients/cats/SearchResultTest.java
+++ src/test/java/brv/telegram/bots/core/restclients/cats/SearchResultTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class SearchResultTest {
 
 	@Test
-	private void test() {
+	public void test() {
 		SearchResult result = new SearchResult();
 		result.setId("id");
 		
