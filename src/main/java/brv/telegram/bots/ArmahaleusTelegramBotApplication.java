ff794e28d483a3d67aea8858115b1d9a30752fd6
Flashky <mevengar@gmail.com>
2020-04-30 17:50:08 +0200

    Initial commit

---
+++ src/main/java/brv/telegram/bots/ArmahaleusTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package brv.telegram.bots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmahaleusTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmahaleusTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/brv/app/ArmahaleusTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package brv.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmahaleusTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
