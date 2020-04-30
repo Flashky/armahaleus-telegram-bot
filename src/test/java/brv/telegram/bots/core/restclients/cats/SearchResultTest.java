c183a8ebc99219eebca9b51a970d417c7d078da2
Flashky <mevengar@gmail.com>
2020-04-30 20:18:32 +0200

    Ajustadas clases de tests

---
+++ src/test/java/brv/telegram/bots/core/restclients/cats/SearchResultTest.java
@@ -0,0 +1,17 @@
+package brv.telegram.bots.core.restclients.cats;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SearchResultTest {
+
+	@Test
+	void test() {
+		SearchResult result = new SearchResult();
+		result.setId("id");
+		
+		assertEquals("id", result.getId());
+	}
+
+}
