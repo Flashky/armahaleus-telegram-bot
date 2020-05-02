03deefb786d4b83500e7d3066eaffe9fe11bec94
Flashky <mevengar@gmail.com>
2020-05-02 09:57:21 +0200

    Implementada esqueleto de método sendRandomCatPhoto

--- src/main/java/brv/telegram/bots/core/ArmahaleusBot.java
+++ src/main/java/brv/telegram/bots/core/ArmahaleusBot.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Component;
 import org.telegram.abilitybots.api.bot.AbilityBot;
 import org.telegram.abilitybots.api.objects.Ability;
 import org.telegram.abilitybots.api.objects.Locality;
+import org.telegram.abilitybots.api.objects.MessageContext;
 import org.telegram.abilitybots.api.objects.Privacy;
 import org.telegram.abilitybots.api.toggle.BareboneToggle;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 public class ArmahaleusBot extends AbilityBot {
@@ -29,11 +32,28 @@ public class ArmahaleusBot extends AbilityBot {
 	        .privacy(Privacy.PUBLIC)
 	        .locality(Locality.ALL)
 	        .input(0)
-	        .action(ctx -> {
-	        	System.out.println("paw paw!");
-	        })
+	        .action(ctx -> this.sendRandomCatPhoto(ctx))
 	        .build();
-}
+	}
+	
+	private void sendRandomCatPhoto(MessageContext ctx) {
+		
+		try {
+			
+			SendPhoto photo = new SendPhoto();
+			photo.setChatId(ctx.chatId());
+			photo.setPhoto("https://img.freepik.com/foto-gratis/gato-mojado-bano_160901-244.jpg");
+	
+			sender.sendPhoto(photo);
+		} catch (TelegramApiException e) {
+
+			silent.send("Sorry, I can't give you any kittens right now.", ctx.chatId());
+			e.printStackTrace();
+			
+		}
+	}
+	
+	
 
 
 	
