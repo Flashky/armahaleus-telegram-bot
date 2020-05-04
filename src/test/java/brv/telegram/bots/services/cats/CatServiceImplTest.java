fe6ea2489842acaaa799c7f8abe6e9f5286e739e
Flashky <mevengar@gmail.com>
2020-05-04 18:50:16 +0200

    Created test for CatServiceImpl class

---
+++ src/test/java/brv/telegram/bots/services/cats/CatServiceImplTest.java
@@ -0,0 +1,100 @@
+package brv.telegram.bots.services.cats;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import brv.telegram.bots.restclients.cats.CatApiClient;
+import brv.telegram.bots.restclients.cats.Image;
+import brv.telegram.bots.services.common.dto.Media;
+import brv.telegram.bots.services.common.dto.MediaType;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CatServiceImplTest {
+	
+	private final static String GIF_IMAGE_URL = "http://random-image.com/cat.gif";
+	private final static String JPG_IMAGE_URL = "http://random-image.com/cat.jpg";
+	private final static String PNG_IMAGE_URL = "http://random-image.com/cat.png";
+	private final static String BMP_IMAGE_URL = "http://random-image.com/cat.bmp";
+	private final static String MP4_VIDEO_URL = "http://random-image.com/cat.mp4";
+	
+	@InjectMocks
+	private CatService catService = new CatServiceImpl();
+	
+	@Mock
+	private CatApiClient catApiClient;
+
+	@Test
+	public void getRandomCatAsGifTest() {
+		
+		// Prepare pojos
+		Image image = new Image();
+		image.setUrl(GIF_IMAGE_URL);
+		
+		// Prepare mocks
+		Mockito.doReturn(image).when(catApiClient).getRandomCatImage();
+		
+		// Execute test
+		Media result = catService.getRandomCat();
+		
+		// Assertions
+		assertEquals(MediaType.GIF, result.getType());
+		assertEquals(GIF_IMAGE_URL, result.getLink().getHref());
+	}
+
+	@Test
+	public void getRandomCatAsMp4Test() {
+		
+		// Prepare pojos
+		Image image = new Image();
+		image.setUrl(MP4_VIDEO_URL);
+		
+		// Prepare mocks
+		Mockito.doReturn(image).when(catApiClient).getRandomCatImage();
+		
+		// Execute test
+		Media result = catService.getRandomCat();
+		
+		// Assertions
+		assertEquals(MediaType.VIDEO, result.getType());
+		assertEquals(MP4_VIDEO_URL, result.getLink().getHref());
+	}
+	
+	@Test
+	public void getRandomCatAsJpgTest() {
+		getRandomCatAsImageTest(JPG_IMAGE_URL);
+	}
+
+	@Test
+	public void getRandomCatAsPngTest() {
+		getRandomCatAsImageTest(PNG_IMAGE_URL);
+	}
+	
+	@Test
+	public void getRandomCatAsBmpTest() {
+		getRandomCatAsImageTest(BMP_IMAGE_URL);
+	}
+	
+	private void getRandomCatAsImageTest(String expectedImageUrl) {
+		
+		// Prepare pojos
+		Image image = new Image();
+		image.setUrl(expectedImageUrl);
+		
+		// Prepare mocks
+		Mockito.doReturn(image).when(catApiClient).getRandomCatImage();
+		
+		// Execute test
+		Media result = catService.getRandomCat();
+		
+		// Assertions
+		assertEquals(MediaType.IMAGE, result.getType());
+		assertEquals(expectedImageUrl, result.getLink().getHref());
+		
+	}
+}
