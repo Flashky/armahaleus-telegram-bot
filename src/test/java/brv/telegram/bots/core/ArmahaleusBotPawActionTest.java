a200e0f8de2422dd4366a0065a61613b7560b5f3
Flashky <mevengar@gmail.com>
2020-05-04 20:13:39 +0200

    Added support for cat gifs and videos

--- src/test/java/brv/telegram/bots/core/ArmahaleusBotPawActionTest.java
+++ src/test/java/brv/telegram/bots/core/ArmahaleusBotPawActionTest.java
@@ -2,22 +2,29 @@ package brv.telegram.bots.core;
 
 import static org.mockito.Mockito.times;
 
+import java.util.Optional;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.telegram.abilitybots.api.objects.MessageContext;
 import org.telegram.abilitybots.api.sender.MessageSender;
 import org.telegram.abilitybots.api.sender.SilentSender;
+import org.telegram.telegrambots.meta.api.methods.send.SendAnimation;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import brv.telegram.bots.restclients.cats.CatApiClient;
-import brv.telegram.bots.restclients.cats.Image;
+import brv.telegram.bots.services.cats.CatService;
+import brv.telegram.bots.services.common.dto.Link;
+import brv.telegram.bots.services.common.dto.Media;
+import brv.telegram.bots.services.common.dto.MediaType;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
@@ -28,8 +35,13 @@ public class ArmahaleusBotPawActionTest {
 
 	private static PodamFactory podamFactory = new PodamFactoryImpl();
 	
+	private final static String GIF_IMAGE_URL = "http://random-image.com/cat.gif";
+	private final static String JPG_IMAGE_URL = "http://random-image.com/cat.jpg";
+	private final static String MP4_VIDEO_URL = "http://random-image.com/cat.mp4";
+	
 	// It is important to keep the bot static to avoid any file lock issues
 	@InjectMocks
+	@Spy
 	private static ArmahaleusBot bot = new ArmahaleusBot("bot-token", "bot-name");
 	
 	@Mock
@@ -39,29 +51,96 @@ public class ArmahaleusBotPawActionTest {
 	private SilentSender silent;
 	
 	@Mock
-	private CatApiClient catApiClient;
+	private CatService catService;
 	
 	@Before
 	public void setUp() {
 		
-		Image image = podamFactory.manufacturePojo(Image.class);
-		Mockito.doReturn(image).when(catApiClient).getRandomCatImage();
-		
 	}
 	
 	@Test
-	public void pawActionTest() throws Exception{
+	public void pawActionaAsImageTest() throws Exception{
 
 		// Prepare context pojos
 		Update update = new Update();
 		User user = podamFactory.manufacturePojo(User.class);
 		MessageContext context = MessageContext.newContext(update, user, CHAT_ID);
 
+		Media media = new Media();
+		media.setType(MediaType.IMAGE);
+		
+		Link link = new Link();
+		link.setHref(JPG_IMAGE_URL);
+		media.setLink(link);
+		
+		Optional<Media> result = Optional.of(media);
+		
+		// Prepare mocks
+		Mockito.doReturn(result).when(catService).getRandomCat();
+		
 		// We consume a context in the lamda declaration, so we pass the context to the action logic
 		bot.paw().action().accept(context);
 
 		// We verify that the silent sender was called only ONCE and sent Hello World to CHAT_ID!
 		Mockito.verify(sender, times(1)).sendPhoto(Mockito.any());
+		Mockito.verify(silent, times(0)).send(Mockito.any(), Mockito.anyLong());
+	}
+	
+	@Test
+	public void pawActionaAsGifTest() throws Exception{
+
+		// Prepare context pojos
+		Update update = new Update();
+		User user = podamFactory.manufacturePojo(User.class);
+		MessageContext context = MessageContext.newContext(update, user, CHAT_ID);
+
+		Media media = new Media();
+		media.setType(MediaType.GIF);
+		
+		Link link = new Link();
+		link.setHref(GIF_IMAGE_URL);
+		media.setLink(link);
+		
+		Optional<Media> result = Optional.of(media);
+		
+		// Prepare mocks
+		Mockito.doReturn(result).when(catService).getRandomCat();
+		Mockito.doReturn(new Message()).when(bot).execute(Mockito.any(SendAnimation.class));
+
+		
+		// We consume a context in the lamda declaration, so we pass the context to the action logic
+		bot.paw().action().accept(context);
+
+		// We verify that the silent sender was called only ONCE and sent Hello World to CHAT_ID!
+		Mockito.verify(bot, times(1)).execute(Mockito.any(SendAnimation.class));
+		Mockito.verify(silent, times(0)).send(Mockito.any(), Mockito.anyLong());
+	}
+	
+	@Test
+	public void pawActionaAsVideoTest() throws Exception{
+
+		// Prepare context pojos
+		Update update = new Update();
+		User user = podamFactory.manufacturePojo(User.class);
+		MessageContext context = MessageContext.newContext(update, user, CHAT_ID);
+
+		Media media = new Media();
+		media.setType(MediaType.VIDEO);
+		
+		Link link = new Link();
+		link.setHref(MP4_VIDEO_URL);
+		media.setLink(link);
+		
+		Optional<Media> result = Optional.of(media);
+		
+		// Prepare mocks
+		Mockito.doReturn(result).when(catService).getRandomCat();
+		
+		// We consume a context in the lamda declaration, so we pass the context to the action logic
+		bot.paw().action().accept(context);
+
+		// We verify that the silent sender was called only ONCE and sent Hello World to CHAT_ID!
+		Mockito.verify(sender, times(1)).sendVideo(Mockito.any());
 		Mockito.verify(silent, times(0)).send(Mockito.any(), Mockito.anyLong());
 	}
 	
@@ -73,6 +152,18 @@ public class ArmahaleusBotPawActionTest {
 		User user = podamFactory.manufacturePojo(User.class);
 		MessageContext context = MessageContext.newContext(update, user, CHAT_ID);
 
+		Media media = new Media();
+		media.setType(MediaType.IMAGE);
+		
+		Link link = new Link();
+		link.setHref(JPG_IMAGE_URL);
+		media.setLink(link);
+		
+		Optional<Media> result = Optional.of(media);
+		
+		// Prepare mocks
+		Mockito.doReturn(result).when(catService).getRandomCat();
+		
 		// Prepare mock to test this exception being thrown
 		Mockito.doThrow(TelegramApiException.class).when(sender).sendPhoto(Mockito.any());
 
