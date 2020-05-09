0aeb80a7d92f5f3050610c40c6722f1d734802d2
Flashky <mevengar@gmail.com>
2020-05-09 12:14:51 +0200

    Add file size in bytes to the Media and DogMedia DTOs

--- src/main/java/brv/telegram/bots/services/dogs/DogServiceImpl.java
+++ src/main/java/brv/telegram/bots/services/dogs/DogServiceImpl.java
@@ -4,12 +4,14 @@ import java.util.Optional;
 
 import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import brv.telegram.bots.restclients.randomdog.DogMedia;
 import brv.telegram.bots.restclients.randomdog.RandomDogApiClient;
 import brv.telegram.bots.services.common.dto.Media;
 import brv.telegram.bots.services.dogs.mappers.RandomDogMapper;
 
+@Service
 public class DogServiceImpl implements DogService {
 
 	@Autowired
